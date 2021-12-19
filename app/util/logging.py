9fbd3dec0435bb83a2429cdd1c7f9b835bc5022e
vernierm <verniermt@gmail.com>
2021-12-19 15:49:14 +0100

    add timestamp to log

--- app/util/logging.py
+++ app/util/logging.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 from app.util.file_util import prepare_file_path, assert_not_exists, file_basename, assert_exists, dump_to_file, \
     read_json_from_file
+
+TEXT = 'text'
+TIMESTAMP = 'timestamp'
 
 LOGS = 'logs'
 TITLE = 'title'
@@ -22,8 +27,13 @@ def initialize_log_file(file_name):
 def append_to_file(file_name, text):
     file_path = prepare_file_path(file_name)
     assert_exists(file_path)
+
     json_data = read_json_from_file(file_path)
-    json_data[LOGS].append(text)
+    now = datetime.now()
+    json_data[LOGS].append({
+        TIMESTAMP: (now.strftime("%d-%m-%Y %H:%M:%S")),
+        TEXT: text
+    })
     dump_to_file(file_path, json_data)
 
 
@@ -31,4 +41,13 @@ def print_logs_from_file(file_name):
     file_path = prepare_file_path(file_name)
     assert_exists(file_path)
     json_data = read_json_from_file(file_path)
-    print(*json_data[LOGS], sep='\n')
+    formatted_logs = format_output(json_data[LOGS])
+    print(*formatted_logs, sep='\n')
+
+
+def format_output(logs):
+    return [format_single_output(log) for log in logs]
+
+
+def format_single_output(log):
+    return '[{}] {}'.format(log[TIMESTAMP], log[TEXT])
